3517d02025e7277a0f3a49ef66aaadd198028160
eastshin <dongyoon0201@pusan.ac.kr>
2025-09-08 19:57:12 +0900

    Programming assginment 1

---
+++ helloworld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    // printf is a function from <stdio.h> that prints formatted text to the console.
+    // The '\n' is a special character that represents a newline.
+    printf("Hello, World!\n");
+
+    // The main function returns 0 to indicate that the program executed successfully.
+    return 0;
+}
